223a7e95b65ef391eb0d2df348ddbeb021989d7e
rnng <g.lehker@gmail.com>
2014-04-14 14:10:01 +0200

    Edited connect.java

--- src/database/connect.java
+++ src/database/connect.java
@@ -3,13 +3,18 @@ import java.sql.*;
 
 class connect {
 	private static Connection con;
+	private static String dbUser ="adminbsTSA3H";
+	private static String dbPw = "6_76lpJkIE8V";
+	private static String dbPort = "3306";
+	private static String dbHost="localhost";
+	private static String dbName="tasks";
 	
 	private connect(){}
 	
 	public static Connection getConnection(){
 		if (con == null){
 			try {
-				con = DriverManager.getConnection("jdbc:mysql://localhost/tasks", "adminbsTSA3H", "adminbsTSA3H");
+				con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+dbPort+"/"+dbName, dbUser, dbPw);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
