17e6a1cfe150732d5dfeee0136c21bb7b2898c88
rnng <g.lehker@gmail.com>
2014-04-20 17:00:03 +0200

    Signed-off-by: rnng <g.lehker@gmail.com>

---
+++ src/database/Connect.java
@@ -0,0 +1,28 @@
+package database;
+import java.sql.*;
+     
+class Connect {
+	private static Connection con;
+	private static String dbUser ="%";
+	private static String dbPw = "%";
+	private static String dbPort = "3306";
+	private static String dbHost="localhost";
+	private static String dbName="tasks";
+	
+	private Connect(){}
+	
+	public static Connection getConnection() throws SQLException{
+		if (con == null){
+			try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+			} catch (Exception e) {
+				return null;
+			} 
+			con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+dbPort+"/"+dbName, dbUser, dbPw);
+		}
+		return con;
+	}
+	public static void releaseConnection() throws SQLException{
+		con.close();
+	}
+}
