33d9a302ea79f514b7977290f6f4b422b20fa39f
rnng <g.lehker@gmail.com>
2014-04-20 16:46:57 +0200

    Added Throw-Exceptions for all Operations

--- src/database/Queries.java
+++ src/database/Queries.java
@@ -5,32 +5,21 @@ import java.sql.*;
 public class Queries {
 	private Queries(){}
 	
-	public static Object scalarQuery(String table, String column, String where, String param, String condition) throws SQLException{
-		PreparedStatement query;
-		if(where == ""){
-			query = Connect.getConnection().prepareStatement("SELECT "+column+" FROM "+table+" WHERE "+param+" LIKE ?");
-			
-		}else{
-			query = Connect.getConnection().prepareStatement("SELECT "+column+" FROM "+table+" WHERE "+where +" AND "+param+" LIKE ?");
-		}
-		ResultSet rs = query.executeQuery();
-		return rs.getObject(0);
-	}
+	public static Object scalarQuery(String table, String column, String where) throws SQLException{
+			PreparedStatement query;
+			query = Connect.getConnection().prepareStatement("SELECT "+column+" FROM "+table+" WHERE "+where);
+			ResultSet rs = query.executeQuery();
+			return rs.getObject(0);	}
 	
 	public static Object  scalarQuery(String sql) throws SQLException{
-		Statement query = Connect.getConnection().createStatement();
-		ResultSet rs = query.executeQuery(sql);
-		return rs.getObject(0);
+			Statement query = Connect.getConnection().createStatement();
+			ResultSet rs = query.executeQuery(sql);
+			return rs.getObject(0);
 	}
 	
-	public static ResultSet rowQuery(String column, String table, String where, String param, String condition) throws SQLException{
+	public static ResultSet rowQuery(String column, String table, String where) throws SQLException{
 		PreparedStatement query;
-		if(where == ""){
-			query = Connect.getConnection().prepareStatement("SELECT "+column+" FROM "+table+" WHERE "+param+" LIKE ?");
-			
-		}else{
-			query = Connect.getConnection().prepareStatement("SELECT "+column+" FROM "+table+" WHERE "+where +" AND "+param+" LIKE ?");
-		}
+		query = Connect.getConnection().prepareStatement("SELECT "+column+" FROM "+table+" WHERE "+where);
 		ResultSet rs = query.executeQuery();
 		return rs;
 	}
@@ -38,79 +27,52 @@ public class Queries {
 	public static ResultSet rowQuery(String sql) throws SQLException{
 		Statement query = Connect.getConnection().createStatement();
 		ResultSet rs = query.executeQuery(sql);
-		return rs;		
+		return rs;	
 	}
 	
 	public static boolean updateQuery(String table, String updateString, String where) throws SQLException{
-		try{
-			PreparedStatement query = Connect.getConnection().prepareStatement("UPDATE "+table+" SET "+updateString+" WHERE "+where);
-			query.executeQuery();
-			return(query.getUpdateCount()>0);
-		}catch(SQLException e){
-			return false;
-		}	
+		PreparedStatement query = Connect.getConnection().prepareStatement("UPDATE "+table+" SET "+updateString+" WHERE "+where);
+		query.executeQuery();
+		return(query.getUpdateCount()>0);
 	}
 	
-	public static boolean updateQuery(String sql){
-		try{
-			Statement query = Connect.getConnection().createStatement();
-			query.executeUpdate(sql);
-			return (query.getUpdateCount()>0);
-		}catch (SQLException e){
-			return false;
-		}	
+	public static boolean updateQuery(String sql) throws SQLException{
+		Statement query = Connect.getConnection().createStatement();
+		query.executeUpdate(sql);
+		return (query.getUpdateCount()>0);
 	}
 
-	public static boolean deleteQuery(String table, String where){
-		try{
-			PreparedStatement query = Connect.getConnection().prepareStatement("DELETE FROM "+table+" WHERE "+where);
-			query.executeQuery();
-			return true;
-		}catch(SQLException e){
-			return false;
-		}
+	public static boolean deleteQuery(String table, String where) throws SQLException{
+		PreparedStatement query = Connect.getConnection().prepareStatement("DELETE FROM "+table+" WHERE "+where);
+		query.executeQuery();
+		return true;
 	}
 	 
 	public static boolean deleteQuery(String sql) throws SQLException{
-		try{
-			Statement query = Connect.getConnection().createStatement();
-			query.executeUpdate(sql);
-			return (query.getUpdateCount()>0);
-		}catch (SQLException e){
-			return false;
-		}		
+		Statement query = Connect.getConnection().createStatement();
+		query.executeUpdate(sql);
+		return (query.getUpdateCount()>0);
 	}
 	
-	public static int insertQuery(String table, String columns, String values){
-		try{
-			PreparedStatement query = Connect.getConnection().prepareStatement("INSERT INTO "+table+" ("+columns+") VALUES ("+values+")");
-			query.executeQuery();
-			ResultSet generatedKeys = query.getGeneratedKeys();
-			if (generatedKeys.next()) {
-	            return generatedKeys.getInt(1);
-	        } else {
-	            throw new SQLException("Creating row failed, no generated key obtained.");
-	        }
-		}catch(SQLException e){
-			return -1;
-		}
+	public static int insertQuery(String table, String columns, String values) throws SQLException{
+		PreparedStatement query = Connect.getConnection().prepareStatement("INSERT INTO "+table+" ("+columns+") VALUES ("+values+")");
+		query.executeQuery();
+		ResultSet generatedKeys = query.getGeneratedKeys();
+		if (generatedKeys.next()) {
+            return generatedKeys.getInt(1);
+        } else {
+            throw new SQLException("Creating row failed, no generated key obtained.");
+        }
 	}
 	
 	public static int insertQuery(String sql) throws SQLException{
-		try{
-			Statement query = Connect.getConnection().createStatement();
-			query.executeUpdate(sql);
-			ResultSet generatedKeys = query.getGeneratedKeys();
-			if (generatedKeys.next()) {
-	            return generatedKeys.getInt(1);
-	        } else {
-	            throw new SQLException("Creating row failed, no generated key obtained.");
-	        }
-		}catch (SQLException e){
-			return -1;
-		}	
+		Statement query = Connect.getConnection().createStatement();
+		query.executeUpdate(sql);
+		ResultSet generatedKeys = query.getGeneratedKeys();
+		if (generatedKeys.next()) {
+            return generatedKeys.getInt(1);
+        } else {
+            throw new SQLException("Creating row failed, no generated key obtained.");
+        }
 	}
-	
-	
-
 }
