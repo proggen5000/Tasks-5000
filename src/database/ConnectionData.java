124548852d7d72043819b912cb222d20cf56c32d
rnng <g.lehker@gmail.com>
2014-04-24 21:28:26 +0200

    Delete ConnectionData.java

--- src/database/ConnectionData.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-class ConnectionData {
-	static final String dbUser ="adminbsTSA3H";
-	static final String dbPw = "6_76lpJkIE8V";
-	static final String dbPort = "3306";
-	static final String dbHost="localhost";
-	static final String dbName="tasks";
-}
