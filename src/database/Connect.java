5ab263b6c4763c45ab9affe0719a2f40cbe242d6
rnng <g.lehker@gmail.com>
2014-04-24 21:25:39 +0200

    Extracted Connection-Attributes to ConnectionData.java

--- src/database/Connect.java
+++ src/database/Connect.java
@@ -3,11 +3,6 @@ import java.sql.*;
 
 class Connect {
 	private static Connection con;
-	private static String dbUser ="adminbsTSA3H";
-	private static String dbPw = "6_76lpJkIE8V";
-	private static String dbPort = "3306";
-	private static String dbHost="localhost";
-	private static String dbName="tasks";
 	
 	private Connect(){}
 	
@@ -18,7 +13,7 @@ class Connect {
 			} catch (Exception e) {
 				return null;
 			} 
-			con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+dbPort+"/"+dbName, dbUser, dbPw);
+			con = DriverManager.getConnection("jdbc:mysql://"+ConnectionData.dbHost+":"+ConnectionData.dbPort+"/"+ConnectionData.dbName, ConnectionData.dbUser, ConnectionData.dbPw);
 		}
 		return con;
 	}
---
+++ src/database/ConnectionData.java
@@ -0,0 +1,9 @@
+package database;
+
+class ConnectionData {
+	static final String dbUser ="adminbsTSA3H";
+	static final String dbPw = "6_76lpJkIE8V";
+	static final String dbPort = "3306";
+	static final String dbHost="localhost";
+	static final String dbName="tasks";
+}
