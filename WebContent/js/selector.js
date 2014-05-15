07449dd3576da0bb98fffdc83a6e7516c112163f
Felix Fichte <dev@spruce.de>
2014-05-15 19:51:05 +0200

    fixes #32
    Ich füge die Handler erst nach dem Laden des Dokuments hinzu

--- WebContent/js/selector.js
+++ WebContent/js/selector.js
@@ -1,13 +1,14 @@
 /**
  * for selecting team members, http://jsfiddle.net/eUDRV/3/
  */
+$(function(){
+	$("#btnLeft").click(function () {
+		var selectedItem = $("#rightValues option:selected");
+		$("#leftValues").append(selectedItem);
+	});
 
-$("#btnLeft").click(function () {
-	var selectedItem = $("#rightValues option:selected");
-	$("#leftValues").append(selectedItem);
-});
-
-$("#btnRight").click(function () {
-	var selectedItem = $("#leftValues option:selected");
-	$("#rightValues").append(selectedItem);
-});
+	$("#btnRight").click(function () {
+		var selectedItem = $("#leftValues option:selected");
+		$("#rightValues").append(selectedItem);
+	});
+})
