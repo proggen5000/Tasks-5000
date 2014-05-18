85709845cbb029267db87568801450eb4a0c761d
unknown <g.lehker@gmail.com>
2014-05-18 21:06:12 +0200

    Einfügen einer Konfigurationsdatei. Bitte die Daten anpassen.

--- src/database/Connect.java
+++ src/database/Connect.java
@@ -1,10 +1,44 @@
  package database;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 class Connect { // TODO
 	private static Connection con;
+	static String dbUser;
+	static String dbPw;
+	static String dbPort;
+	static String dbHost;
+	static String dbName;
 	
 	private Connect(){}
+	
+	static void load_data(){
+		Properties prop = new Properties();
+		InputStream input = null;
+		try{
+			input = new FileInputStream("WebContent/config/db.config");
+			prop.load(input);
+			dbUser = prop.getProperty("dbUser");
+			dbPw = prop.getProperty("dbPw");
+			dbPort = prop.getProperty("dbPort");
+			dbHost = prop.getProperty("dbHost");
+			dbName = prop.getProperty("dbName");
+			
+		}catch(IOException e){
+			System.err.println("Connect.java konnte die Datenbankkonfigurationsdatei nicht finden / lesen");
+		}finally{
+			if(input != null){
+				try{
+					input.close();
+				}catch(IOException e){
+					System.err.println("Fehler beim Schließen der Datei. Bitte kontrollieren Sie die Datei und fahren fort.");
+				}
+			}
+		}
+	}
 	
 	/**
 	 * Stellt eine Verbindung zur Datenbank her.
@@ -12,13 +46,14 @@ class Connect { // TODO
 	 * @exception SQLException Wird geworfen bei Fehlern mit der Verbindung.
 	 */
 	public static Connection getConnection() throws SQLException{
+		load_data();
 		if (con == null){
 			try {
 				Class.forName("com.mysql.jdbc.Driver").newInstance();
 			} catch (Exception e) {
 				return null;
 			} 
-			con = DriverManager.getConnection("jdbc:mysql://"+ConnectionData.dbHost+":"+ConnectionData.dbPort+"/"+ConnectionData.dbName, ConnectionData.dbUser, ConnectionData.dbPw);
+			con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+dbPort+"/"+dbName, dbUser, dbPw);
 		}
 		return con;
 	}
